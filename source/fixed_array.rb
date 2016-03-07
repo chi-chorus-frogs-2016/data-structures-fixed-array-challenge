33f1bab283f30bddf1b92702f1cd93f4bfaf92c8
loganbresnahan <loganbbres@gmail.com>
2016-03-07 15:15:18 -0600

    Finish fixed array with rspec tests

--- source/fixed_array.rb
+++ source/fixed_array.rb
@@ -1,3 +1,21 @@
 class FixedArray
+  attr_reader :size
+
+  def initialize(capacity = 4)
+    @array = Array.new(capacity, nil)
+    @size = capacity
+  end
+
+  def get(index)
+    raise OutOfBoundsException if index > @array.size || index < 0
+    @array[index]
+  end
+
+  def set(index, element)
+    raise OutOfBoundsException if index > @array.size || index < 0
+    @array[index] = element
+  end
 
 end
+
+class OutOfBoundsException < StandardError; end
--- source/fixed_array_spec.rb
+++ source/fixed_array_spec.rb
@@ -1,5 +1,25 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+
+  describe '#initialize' do
+    let(:array) { FixedArray.new }
+    it 'makes a new array with space for size elements' do
+      expect(array.size).to eq(4)
+    end
+  end
+
+  describe 'get' do
+    let(:array) {  FixedArray.new }
+    it 'will return the element at a given index' do
+      expect(array.get(0)).to eq(nil)
+    end
+  end
+
+  describe 'set' do
+    let(:array) { FixedArray.new }
+    it 'will set a given element in the array' do
+      expect(array.set(0, "hyes")).to eq("hyes")
+    end
+  end
 end
